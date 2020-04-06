ab140eda7b284ef4e8664d6d11433730c989653a
AdRusinek <rusinekadrian19@gmail.com>
2020-04-06 03:14:54 +0200

    init project jms

---
+++ src/test/java/com/rusinek/javamessaging/JavaMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusinek.javamessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6dde0540b35cb9de79cbca5d1875e8860e8a496b
AdRusinek <rusinekadrian19@gmail.com>
2020-04-06 04:07:12 +0200

    send and receive of jms messages

--- src/main/java/com/rusinek/javamessaging/listener/HelloMessageListener.java
+++ src/main/java/com/rusinek/javamessaging/listener/HelloMessageListener.java
@@ -2,26 +2,47 @@ package com.rusinek.javamessaging.listener;
 
 import com.rusinek.javamessaging.config.JmsConfig;
 import com.rusinek.javamessaging.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 /**
  * Created by Adrian Rusinek on 06.04.2020
  **/
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers, Message message) {
 
-        System.out.println("I got message!");
+//        System.out.println("I got message!");
 
-        System.out.println(helloWorldMessage);
+//        System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RECEIVE_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Czesc")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
--- src/main/java/com/rusinek/javamessaging/sender/HelloSender.java
+++ src/main/java/com/rusinek/javamessaging/sender/HelloSender.java
@@ -1,12 +1,18 @@
 package com.rusinek.javamessaging.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rusinek.javamessaging.config.JmsConfig;
 import com.rusinek.javamessaging.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 /**
@@ -17,11 +23,10 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
-        System.out.println("I'm sending message");
-
         HelloWorldMessage message = HelloWorldMessage
                 .builder()
                 .id(UUID.randomUUID())
@@ -30,6 +35,32 @@ public class HelloSender {
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
-        System.out.println("Message sent");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+        System.out.println("I'm sending message");
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello ")
+                .build();
+
+      Message receivedMessage =  jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RECEIVE_QUEUE, (Session session) -> {
+            Message helloMessage = null;
+            try {
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                helloMessage.setStringProperty("_type", "com.rusinek.javamessaging.model.HelloWorldMessage");
+
+                System.out.println("Sending hello.");
+
+                return helloMessage;
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            throw new JMSException("wops");
+        });
+        System.out.println(receivedMessage.getBody(String.class));
     }
 }
